ad9c7a9a25d4913e3e1ba4c77dbf94c4a5cd9353
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-05-07 13:56:07 -0300

    UPD the src/Tyst.jl

--- src/Tyst.jl
+++ src/Tyst.jl
@@ -1,5 +1,23 @@
 module Tyst
 
-# Write your package code here.
+using Base64
+
+function obscure(answer::String; key="Tyst")
+    bytes = transcode(UInt8, answer)
+    keybytes = transcode(UInt8, repr(hash(key))[3:end])
+    keystream = repeat(keybytes, ceil(Int, length(bytes) / length(keybytes)))[1:length(bytes)]
+    obscured_bytes = xor.(bytes, keystream)
+    return base64encode(obscured_bytes)
+end
+
+function reveal(scanned::String; key="Tyst")
+    decoded = base64decode(scanned)
+    keybytes = transcode(UInt8, repr(hash(key))[3:end])
+    keystream = repeat(keybytes, ceil(Int, length(decoded) / length(keybytes)))[1:length(decoded)]
+    revealed_bytes = xor.(decoded, keystream)
+    return String(revealed_bytes)
+end
+
+export obscure, reveal
 
 end
