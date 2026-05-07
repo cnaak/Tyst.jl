44ce246d32e7b6f830b3004ffcc60dd21eaadbbe
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-05-07 14:20:30 -0300

    UPD the src/Tyst.jl

--- src/Tyst.jl
+++ src/Tyst.jl
@@ -2,7 +2,7 @@ module Tyst
 
 using Base64
 
-function obscure(answer::String; key="Tyst")
+function obscure(answer::String; key = "Tyst")
     bytes = transcode(UInt8, answer)
     keybytes = transcode(UInt8, repr(hash(key))[3:end])
     keystream = repeat(keybytes, ceil(Int, length(bytes) / length(keybytes)))[1:length(bytes)]
@@ -10,7 +10,7 @@ function obscure(answer::String; key="Tyst")
     return base64encode(obscured_bytes)
 end
 
-function reveal(scanned::String; key="Tyst")
+function reveal(scanned::String; key = "Tyst")
     decoded = base64decode(scanned)
     keybytes = transcode(UInt8, repr(hash(key))[3:end])
     keystream = repeat(keybytes, ceil(Int, length(decoded) / length(keybytes)))[1:length(decoded)]
@@ -19,5 +19,55 @@ function reveal(scanned::String; key="Tyst")
 end
 
 export obscure, reveal
+
+PARTS = (
+    DOCHDR = """
+	#import "@preview/based:0.2.0": base64
+
+    #set page(
+      paper: "a4",
+      margin: (x: 20mm, top: 20mm, bottom: 25mm),
+      numbering: "– 1 –"
+    )
+    #set text(font: "IBM Plex Mono", size: 9.5pt, lang: "pt")
+    #set par(justify: true, leading: .75em)
+
+    #show math.equation: it => {
+      text(font: "STIX Two Math", size: 0.90em)[#box[#it]]
+    }
+    """,
+    QUEHDR = """
+    #import "@preview/zebra:0.1.0": qrcode
+
+    #let the-qcode-raw = qrcode(width: 4.0em, "@OBSCURED@")
+
+    #table(
+      columns: (10%, 10%, 50%, 10%, 20%),
+      align: (
+        left + horizon,
+        left + horizon,
+        left + bottom,
+        center + horizon,
+        right + bottom
+      ),
+      stroke: none,
+      table.cell(rowspan: 2)[#the-qcode-raw],
+      [#box(height: 1.5em)[Nome:]], [#line(length: 100%, stroke: 0.6pt)],
+      [Data:], [#line(length: 100%, stroke: 0.6pt)],
+      [#box(height: 1.5em)[Ass.:]], [#line(length: 100%, stroke: 0.6pt)],
+      [Nota:], [#line(length: 100%, stroke: 0.6pt)],
+    )
+
+    #set text(font: "Libertinus Sans", size: 11pt, lang: "pt")
+    """,
+    QUEIMG = """
+    """,
+	QUEASK = """
+    `[@WEIGHT@]`#h(.5em)@ASK@,
+    #box[$@VARNAME@ = $ #box(stroke: (bottom: 0.6pt), width: 6em) $@UNIT@$]
+	""",
+)
+
+
 
 end
