2b90c4f4a3e06b3dd6446edaf6fc84e2fce346a9
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-05-15 17:05:53 -0300

    UPD the src/Tyst.jl

--- src/Tyst.jl
+++ src/Tyst.jl
@@ -23,21 +23,58 @@ export obscure, reveal
 
 PARTS = (
     DOCHDR = raw"""
-        #import "@preview/based:0.2.0": base64
-        #set page(
-            paper: "a4",
-            margin: (x: 20mm, top: 20mm, bottom: 20mm),
-            numbering: none,
-        )
-        #set text(font: "IBM Plex Mono", size: 9.5pt, lang: "pt")
-        #set par(justify: true, leading: .70em)
-        #show math.equation: it => {
-            text(font: "STIX Two Math", size: 0.90em)[#box[#it]]
-        }
+    #import "@preview/based:0.2.0": base64
+    #set page(
+        paper: "a4",
+        margin: (x: 20mm, top: 20mm, bottom: 20mm),
+        numbering: none,
+    )
+    #set text(font: "IBM Plex Mono", size: 9.5pt, lang: "pt")
+    #set par(justify: true, leading: .70em)
+    #show math.equation: it => {
+        text(font: "STIX Two Math", size: 0.90em)[#box[#it]]
+    }
+    #let ANSKEY = @ANSKEY@
+    #let ANS(body, wid: 8em, hei: 0.7em) = box(
+        stroke: (bottom: 0.6pt),
+        width: wid,
+        height: hei,
+    )[#align(center)[
+        #text(
+            font: "STIX Two Text",
+            fill: red.darken(25%),
+            baseline: -0.2em,
+        )[#if ANSKEY [#body] else [#hide[#body]]]
+    ]]
+    // This determines whether or not answers will be displayed
     """,
-    QUEHDR = raw"""
+    QHDRQR = raw"""
+    """,
+    QUEIMG = raw"""
+    #align(center)[
+      #image("@IMAGEFILE@", width: @IMAGEWIDTH@)
+    ]
+    """,
+    QUEASK = raw"""
+    `[@WEIGHT@]`#h(.75em)@ASK@,#h(.75em)
+    #box[$@VARNAME@ = $ #ANS()[@ANSWER@] $@UNIT@$]
+    """,
+)
+
+# document header generation
+tydhdr(; anskey::Bool = false) = replace(
+    PARTS.DOCHDR,
+    "@ANSKEY@" => anskey ? "true" : "false",
+)
+export tydhdr
+
+# question header template generation
+function QUEHDR(QR::Bool)
+    ret = QR ? raw"""
     #import "@preview/zebra:0.1.0": qrcode
     #let the-qcode-raw = qrcode(width: @QRWIDTH@em, "@OBSCURED@")
+    """ : ""
+    ret *= QR ? raw"""
     #table(
         columns: (@QRWIDTHTAB@em, 1fr, 7fr, 1fr, 2fr),
         align: (
@@ -50,48 +87,44 @@ PARTS = (
         stroke: none,
         inset: 0pt,
         table.cell(rowspan: 3)[#the-qcode-raw],
-        table.cell(colspan: 4)[
-            #box(height: @THIRDHEIGHT@em)[
-            UTFPR-GP
-            #box(width: 1fr)[#align(center)[---]]
-            COEME
-            #box(width: 1fr)[#align(center)[---]]
-            M04
-            #box(width: 1fr)[#align(center)[---]]
-            EM48C
-            #box(width: 1fr)[#align(center)[---]]
-            Geração e Distribuição de Vapor
-            ]
-        ],
-        [#box(height: @THIRDHEIGHT@em)[Nome:]], [#line(length: 100%, stroke: 0.6pt)],
+    """ : raw"""
+    #table(
+        columns: (1fr, 7fr, 1fr, 2fr),
+        align: (
+            left + bottom,
+            left + bottom,
+            center + bottom,
+            right + bottom
+        ),
+        stroke: none,
+        inset: 0pt,
+    """
+    ret *= raw"""
+        table.cell(colspan: 4)[#box(height: 0.8em)[
+            @BANNER@
+        ]],
+        [#box(height: @HEIGHT@em)[Nome:]], [#line(length: 100%, stroke: 0.6pt)],
         [Data:], [#line(length: 100%, stroke: 0.6pt)],
         [Ass.:], [#line(length: 100%, stroke: 0.6pt)],
         [Nota:], [#line(length: 100%, stroke: 0.6pt)],
     )
     #set text(font: "Libertinus Sans", size: 11pt, lang: "pt")
-    """,
-    QUEIMG = raw"""
-    #align(center)[
-      #image("@IMAGEFILE@", width: @IMAGEWIDTH@)
-    ]
-    """,
-    QUEASK = raw"""
-    `[@WEIGHT@]`#h(.75em)@ASK@,#h(.75em)
-    #box[$@VARNAME@ = $ #box(stroke: (bottom: 0.6pt), width: 6em) $@UNIT@$]
-    """,
-)
-
-# document header generation
-tydhdr() = PARTS.DOCHDR
-export tydhdr
+    """
+    return ret
+end
 
 # question header generation
-tyqhdr(obscured::String, ems::Float64=7.5) = replace(
-    PARTS.QUEHDR,
+tyqhdr(obscured::String="ha-ha!";
+    banner::Vector{String},
+    QR::Bool=true,
+    wid::Float64=4.5,
+) = replace(
+    QUEHDR(QR),
     "@OBSCURED@" => obscured,
-    "@QRWIDTH@" => @sprintf("%.2f", ems),
-    "@QRWIDTHTAB@" => @sprintf("%.2f", ems + 0.5),
-    "@THIRDHEIGHT@" => @sprintf("%.2f", ems/3),
+    "@QRWIDTH@" => @sprintf("%.2f", wid),
+    "@QRWIDTHTAB@" => @sprintf("%.2f", wid + 0.8),
+    "@HEIGHT@" => @sprintf("%.2f", (wid - 0.8) / 2),
+    "@BANNER@" => join(banner, "\n        #box(width: 1fr)[#align(center)[---]]\n        "),
 )
 export tyqhdr
 
@@ -111,9 +144,10 @@ export tyqimg
 # question ask generation
 function tyqask(
         ask::String;
-        wgt::Integer = 1,
         var::String,
         uni::String,
+        ans::String,
+        wgt::Integer = 1,
     )
     return replace(
         PARTS.QUEASK,
@@ -121,6 +155,7 @@ function tyqask(
         "@ASK@" => ask,
         "@VARNAME@" => var,
         "@UNIT@" => uni,
+        "@ANSWER@" => ans,
     )
 end
 export tyqask
